2d11affff15aaae2bcef251367b9a81bca48519b
gadiel2311-spec <gadiel2311@gmail.com>
2026-03-03 13:45:19 -0500

    Update project

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.get('/', (req, res) => {
   res
-    .status(200)
+    .status(400)
     .json({ message: 'Hello from the server side! , app: Natours ' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
 });
 
 const port = 3000;
