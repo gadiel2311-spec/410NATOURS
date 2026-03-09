376d442268d121ba8103eb4b01fa4259fb0466fb
gadiel2311-spec <gadiel2311@gmail.com>
2026-03-09 09:14:20 -0400

    Update file paths

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    status: 'success',
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -35,24 +43,23 @@ app.post('/api/v1/tours', (req, res) => {
   //////////////
   tours.push(newTour);
   //
-  
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.status(201).json({
-          status: 'success',
-          data: {
-            tour: newTour,
-          },
-        });
-      }
-    );
-  });
 
-  ////////////////////
-  // res.send('Done');
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+});
 
+////////////////////
+// res.send('Done');
 
 const port = 3000;
 app.listen(port, () => {
