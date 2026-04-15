f1a3c616bac518bea81d36f70413885fcb5262aa
gadiel2311-spec <gadiel2311@gmail.com>
2026-04-15 13:10:28 -0400

    Update project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getALLTours = (req, res) => {
   console.log(req.requestTime);
   ///
@@ -77,12 +87,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null,
