385f16a17e745077f24dd9ed1512566f5103799c
gadiel2311-spec <gadiel2311@gmail.com>
2026-04-21 12:04:32 -0400

    Fix path error

--- app.js
+++ app.js
@@ -6,24 +6,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
-// CORREGIDO: sin espacio al final
-if (process.env.NODE_ENV === 'development') {
+// 1) MIDLEWARES
+if (process.env.NODE_ENV === 'development ') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-// Tu middleware original (lo dejamos igual)
+///
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
 });
 
-// Middleware para imprimir método y URL (para que salga GET /api/v1/tours)
 app.use((req, res, next) => {
-  console.log(req.method, req.originalUrl);
   req.requestTime = new Date().toISOString();
   next();
 });
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-//console.log(process.env);
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
