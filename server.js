a65bf3c748bd0108e81c8e2f36c9a0751fd2721e
gadiel2311-spec <gadiel2311@gmail.com>
2026-04-22 08:47:43 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+const x = 23;
+x = 66;
