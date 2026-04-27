ae129f2a373f5c3263943ec2a95df32ad02551d2
gadiel2311-spec <gadiel2311@gmail.com>
2026-04-27 12:21:19 -0400

    update

--- server.js
+++ server.js
@@ -22,18 +22,36 @@ mongoose
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    requiere: [true, 'A tour must have a name'],
+    required: [true, 'A tour must have a name'],
     unique: true,
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price']
-  }
+    required: [true, 'A tour must have a price'],
+  },
 });
+
+// ✔ MOVER AQUÍ
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💣:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
