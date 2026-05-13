b96258aa4656b09f8f7d1234b09a1b1af114d1a8
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-13 13:19:05 -0400

    Minor changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,10 +14,10 @@ exports.getAllTours = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
-    const tour = await query;
+    const tours = await query;
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
