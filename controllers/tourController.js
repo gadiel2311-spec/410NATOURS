d2c47c3173a90d238b0ca29b0a08bbc3d7d51c5f
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-13 09:58:54 -0400

    Update file paths

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,20 +2,23 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const tours = await Tour.find(queryObj);
+    const query = Tour.find(queryObj);
 
-    // const tours = await Tour.find()
+    // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
 
-    // console.log(tours);
+    // EXECUTE QUERY
+    const tour = await query;
 
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
