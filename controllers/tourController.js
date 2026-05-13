62a52ee0eecf7ea5c912a5658bb52a6be9b19f88
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-13 13:38:58 -0400

    Small improvements

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,17 +4,25 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     // BUILD QUERY
-    // 1) Filtering
+    // 1 A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advance filtering
+    // 1 B) Advance filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join('');
+      query = query.sort(req.query.sort);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
