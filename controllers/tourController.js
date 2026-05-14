af493c077646dc5ee1f4028fadcc628ad26fbfaa
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-14 11:11:05 -0400

    Minor changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,12 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = x;
+      query = query.select('name duration price');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
     // const query = Tour.find()
