889445a97029f5b1eca994dd3f7a803a4715925e
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-18 17:18:59 -0400

    small update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,9 +33,9 @@ exports.getAllTours = async (req, res) => {
     }
 
     // 4) PAGINATION
-
+    const page = req.query.page * 1;
     //page=2&limit=10, 1-10, page1, 11-20, page 2, 21-30 page 3
-    query = query.skip(10).limit(10);
+    query = query.skip(20).limit(10);
 
     // EXECUTE QUERY
     const tours = await query;
