c04a333cb6a7784a2a774ef388c28f244702eb0d
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-27 12:10:55 -0400

    General update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -121,6 +121,7 @@ exports.getTourStart = async (req, res) => {
       {
         $group: {
           _id: null,
+          numTours: { $sum: 1 },
           numRatings: { $sum: '$ratingsQuantity' },
           avgRating: { $avg: '$ratingsAverage' },
           avgPrice: { $avg: '$price' },
