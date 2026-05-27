371251094924ebede1265c57033200252b8a2b11
gadiel2311-spec <gadiel2311@gmail.com>
2026-05-27 11:52:43 -0400

    update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -111,3 +111,34 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStart = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          miniPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(204).json({
+      status: 'fail',
+      message: 'Invalid data sent!🤔',
+    });
+  }
+};
