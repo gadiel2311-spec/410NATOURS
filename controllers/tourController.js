e82c40259253aca9a4faa0d25f845f437cde4004
gadiel2311-spec <gadiel2311@gmail.com>
2026-06-16 09:45:06 -0400

    new codes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 ﻿const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -24,18 +25,19 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = async (req, res, next) => {
-  try {
-    const tour = await Tour.findById(req.params.id);
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
+  //Tour.findOne({ id: req.params.id})
 
-    res.status(200).json({
-      status: 'success',
-      data: { tour },
-    });
-  } catch (err) {
-    next(err);
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
   }
-};
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
 
 ///////////CREATE
 exports.createTour = catchAsync(async (req, res, next) => {
