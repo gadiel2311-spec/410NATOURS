87a28e2233f9a603a2b78673b835ae1726ea414c
gadiel2311-spec <gadiel2311@gmail.com>
2026-06-29 12:52:05 -0400

    Add feature

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -77,6 +78,10 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   }
 
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
 };
 
 const User = mongoose.model('User', userSchema);
