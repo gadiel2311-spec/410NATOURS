7963deb8e1f6f6034ce35f404f486c029b716de7
gadiel2311-spec <gadiel2311@gmail.com>
2026-06-30 10:36:06 -0400

    Add input file

--- utils/email.js
+++ utils/email.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 const sendEmail = (options) => {
   //1) Create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: Process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
