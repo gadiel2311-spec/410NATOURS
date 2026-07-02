1ac4f1f116bfc7aba085069c039af55b0c442880
gadiel2311-spec <gadiel2311@gmail.com>
2026-07-02 08:53:22 -0400

    codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -122,15 +122,20 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password ans passwordConfirm
    to:${resetURL}.\nIf you didn't forget your password, please ignore this email`;
 
-  await sendEmail({
-    email: user.email,
-    subject: 'Your password reset token (vallid for 10 min)',
-    message,
-  });
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
 
-  res.status(200).json({
-    status: 'success',
-    message: 'Token sent to email!',
-  });
+    res.status(200).json({
+      status: 'success',
+      massage: 'Token sent to email',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+  }
 });
 exports.resetPassword = (req, res, next) => {};
