9be138e74d1e7f801791ff60e1945e6ff66d56ff
gadiel2311-spec <gadiel2311@gmail.com>
2026-07-02 09:31:52 -0400

    New codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -136,6 +136,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500,
+    );
   }
 });
 exports.resetPassword = (req, res, next) => {};
